110d2564527177548ea3fd2c357b82e163abc02b
Zara <2019zding@dwight.edu>
2019-03-22 15:35:09 -0400

    first push

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :dpposts
+  resources :highschoolposts
+  # get 'sessions/new'
+  resources :sessions
+  resources :users
+  resources :posts
+  root to: 'static_pages#home'
+  get 'middleSchool', to: 'static_pages#middleSchool'
+  get 'highSchool', to: 'static_pages#highSchool'
+  get 'admin', to: 'static_pages#admin'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
